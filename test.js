04905f63ab81c52e48606bff01492ab271295bd3
Jason Madar <jason@vandrico.com>
2018-06-08 00:16:40 -0700

    adding test file

---
+++ test.js
@@ -0,0 +1,17 @@
+[
+    {
+        title: 'Test lesson',
+        text: [            
+            'This is a test'
+        ]
+    },
+    {
+        title: 'Testing step 2',        
+        text: [
+            `Test`
+        ],
+        condition: () => {
+            return true;
+        }
+    }
+]
